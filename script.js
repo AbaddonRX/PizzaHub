d347a075d8c2ab1a1bed8faeda14a3f7b5cfd33d
Noah Rashed <abaddonrx@gmail.com>
2025-05-05 09:33:40 -0300

    adw

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("scroll", function () {
     const homeSection = document.querySelector(".home");
     const scrollPosition = window.scrollY;
-    homeSection.style.backgroundPositionY = `${scrollPosition * 0.2}px`;
+    homeSection.style.backgroundPositionY = `${scrollPosition * 0.01}px`;
 });
